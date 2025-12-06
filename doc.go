d6db24a231071382d03fb08dc2bc13c127e40982
Asher Buk <2asherbuk@gmail.com>
2025-12-06 19:33:41 +0200

    add package documentation for GoDoc

---
+++ doc.go
@@ -0,0 +1,16 @@
+// Package algods provides educational implementations of classic computer science
+// algorithms and data structures.
+//
+// Educational repository focusing on fundamental CS problems.
+// Clean code with semantic naming for learning purposes.
+//
+// # Usage
+//
+// Each algorithm is in its own package and can be used independently:
+//
+//	import "github.com/asherbuk/AlgoDSgo/algorithms/sort/quicksort"
+//	import "github.com/asherbuk/AlgoDSgo/algorithms/search/binarysearch"
+//	import "github.com/asherbuk/AlgoDSgo/datastructures/graph"
+//
+// For more information, see: https://github.com/AshBuk/ALgoDSgo
+package algods
